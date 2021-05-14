67fb9e3d21a41dae92e8689670b6157ccf3d9954
Sara Cearc <scearc930@gmail.com>
2021-05-14 14:47:56 -0400

    SC: added a runner class and used calculator class

--- Calculator/src/com/qa/calculator/Calculator.java
+++ Calculator/src/com/qa/calculator/Calculator.java
@@ -1,21 +1,6 @@
 package com.qa.calculator;
 
 public class Calculator {
-
-	public static void main(String[] args) {
-		int sum = add(2, 3);
-		System.out.println(sum);
-		
-		int subtract = sub(7, 9);
-		System.out.println(subtract);
-		
-		int multiply = mult(7, 9);
-		System.out.println(multiply);
-		
-		double divde = divd(9.0, 12.0);
-		System.out.println(divde);
-
-	}
 	
 	public static int add(int num1, int num2) {
 		return num1 + num2;
---
+++ Calculator/src/com/qa/calculator/Runner.java
@@ -0,0 +1,13 @@
+package com.qa.calculator;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		int result = Calculator.add(6, 9);
+		
+		System.out.println(result);
+
+	}
+
+}
