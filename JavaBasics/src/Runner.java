564cf2a99b1ba50a623b6163d7f1402cbce162dd
Sara Cearc <scearc930@gmail.com>
2021-05-14 10:54:14 -0400

    added folders and java files to repo

---
+++ JavaBasics/src/Runner.java
@@ -0,0 +1,17 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		int numberVar;
+		char myChar = 'e';
+		
+		numberVar = 250;
+		
+		System.out.println("Hello World!");
+		System.out.println(numberVar);
+		System.out.println(myChar);
+
+	}
+
+}
